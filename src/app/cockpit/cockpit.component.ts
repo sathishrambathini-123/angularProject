26e0ec210342fe624fa03d6cb7ed7d2c57ddc38c
sathishrambathini-123 <58430534+sathishrambathini-123@users.noreply.github.com>
2019-12-02 19:22:29 +0530

    Add files via upload

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  severName : '';
+  severContent : '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(){
+     
+  }
+  onAddBlueprint(){
+
+  }
+
+}
